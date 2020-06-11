be133cb3556d75f5156165e0da53b3f28c9fa0a7
lualua0909 <lua.solution@gmail.com>
2020-06-11 15:45:24 +0700

    commit build

--- src/components/atoms/footer.js
+++ src/components/atoms/footer.js
@@ -3,9 +3,8 @@ import { Layout, Menu, Icon, Avatar } from "antd";
 
 export default function Footer(props) {
   return (
-    <></>
-    // <Layout.Footer style={{ textAlign: "center" }}>
-    //   Ant Design ©2020 Created by NAD
-    // </Layout.Footer>
+    <Layout.Footer style={{ textAlign: "center" }}>
+      Ant Design ©2020 Created by NAD
+    </Layout.Footer>
   );
 }
