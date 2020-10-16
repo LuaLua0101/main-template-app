1e3654b7344958a6d0c5985f0c348307363ae6f0
Duy Anh Nguyen <duyanguyen@deloitte.ca>
2020-10-16 17:59:43 +0700

    .

--- src/googleAuth.js
+++ src/googleAuth.js
@@ -31,14 +31,7 @@ export const googleSignIn = () => {
 
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
+    console.log(user)
   } else {
   }
 });
